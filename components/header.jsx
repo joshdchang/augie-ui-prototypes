c7d7efc7152cae97e60d7d03e007cda6082f7d1a
Josh Chang <joshchang04@gmail.com>
2022-07-15 02:30:58 -0400

    sign up card

--- components/header.jsx
+++ components/header.jsx
@@ -1,13 +1,38 @@
-import Image from 'next/image';
+import Link from "next/link"
 
 export default function Header() {
+
+  const navbar = [
+    {
+      name: 'Login',
+      href: '/login'
+    },
+    {
+      name: 'Learn More',
+      href: '/about'
+    },
+    {
+      name: 'Examples',
+      href: '/examples'
+    },
+  ]
+
   return (
     <div className="relative">
       <div className="h-3 bg-[#FFC18E]"></div>
       <div className="h-3 bg-[#CA4E79]"></div>
       <div className="h-3 bg-[#7A4069] w-[calc(100%-14rem)] rounded-[0_0_1rem_0]"></div>
       <div className="h-3 bg-[#513252] w-[calc(100%-16rem)] rounded-[0_0_1rem_0]"></div>
-      <img src="/augie-logo.svg" className="absolute right-24 w-32 top-8" />
+      <div className="grid gap-8 grid-flow-col justify-start p-8 px-24">
+        {navbar.map(({ name, href }) => (
+          <Link href={href}>
+            <a className="text-slate-300 hover:text-white transition-colors text-lg">{name}</a>
+          </Link>
+        ))}
+      </div>
+      <Link href="/">
+        <img src="/augie-logo.svg" className="absolute right-24 w-32 top-8" />
+      </Link>
     </div>
   )
 }
--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,15 +1,65 @@
 import Head from 'next/head'
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+import { Password } from 'primereact/password';
+import { useState } from 'react';
+import Link from 'next/link';
 
 export default function Signup() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [visible, setVisible] = useState(false)
+  const [loading, setLoading] = useState(false)
+
   return (
     <>
       <Head>
         <title>Sign Up – Augie UI Prototypes</title>
-        <meta name="description" content="Josh's prototypes for the Augie UI he is making while he learns React/Next" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content="Josh's prototypes for the Augie UI he is making while he learns React/Next" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      {/* <h1>Signup</h1> */}
+      <div className='grid justify-items-center'>
+        <div className='bg-slate-800 w-full max-w-lg rounded-2xl p-12 grid gap-8 text-center'>
+          <h2 className='text-2xl font-semibold z-10'>Sign Up and Start Creating!</h2>
+          <div className='text-lg text-slate-300 bg-slate-300 text-center border-b-[1px] border-b-slate-300 leading-[1px] my-4'>
+            <span className='bg-slate-800 p-4'>Create An Account With</span>
+          </div>
+          <div className='grid gap-4 grid-flow-col'>
+            <Button className='p-2 grid justify-center grid-flow-col p-button-secondary p-button-rounded' aria-label='Google'>
+              <i className='pi pi-google px-1'></i>
+              <span className='px-2'>Google</span>
+            </Button>
+            <Button className='p-2 grid justify-center grid-flow-col p-button-secondary p-button-rounded' aria-label='Facebook'>
+              <i className='pi pi-facebook px-1'></i>
+              <span className='px-2'>Facebook</span>
+            </Button>
+            <Button className='p-2 grid justify-center grid-flow-col p-button-secondary p-button-rounded' aria-label='Apple'>
+              <i className='pi pi-apple px-1'></i>
+              <span className='px-2'>Apple</span>
+            </Button>
+          </div>
+          <div className='text-lg text-slate-300 bg-slate-300 text-center border-b-[1px] border-b-slate-300 leading-[1px] my-4'>
+            <span className='bg-slate-800 p-4'>Or Use Your Email</span>
+          </div>
+          <div className='grid gap-4'>
+            <span className='p-input-icon-left'>
+              <i className='pi pi-envelope px-1' />
+              <InputText className='w-full pl-4' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' type='email' />
+            </span>
+            <span className='p-input-icon-left'>
+              <Password className='w-full' inputClassName='w-full pl-10' value={password} onChange={(e) => setPassword(e.target.value)} toggleMask placeholder='Password' />
+              <i className='pi pi-lock px-1' />
+            </span>
+          </div>
+          <Button className='p-button p-button-rounded' disabled={loading} onClick={() => {setLoading(true)}}>
+            { loading ? <i className='pi pi-spin pi-spinner px-1' /> : null }
+            <span className='text-center w-full'>{loading ? 'Loading' : 'Create Account'}</span>
+          </Button>
+          <p className='text-lg text-center text-slate-300'>Already have an account? <Link href='/login'><a className='text-blue-400 mx-1'>Login here</a></Link></p>
+        </div>
+      </div>
     </>
   )
 }
