54a5d20bcbbc8e7a791f25a28c7598ddb0f96eb0
Josh Chang <joshchang04@gmail.com>
2022-07-19 16:33:29 -0400

    add boilerplate for dashboard page

---
+++ pages/dashboard.jsx
@@ -0,0 +1,16 @@
+
+// routes to /dashboard
+
+import Head from 'next/head'
+
+export default function Examples() {
+  return (
+    <>
+      <Head>
+        <title>Dashboard - Augie UI Prototypes</title>
+      </Head>
+
+      <p>Dashboard Page</p>
+    </>
+  )
+}
